0722a944ee4fd01c8fbfc47b1af3e1e9bc29eb07
chloelo <chloelo925@gmail.com>
2026-06-25 15:37:00 +0800

    feat: 🎸 調整 cjs 為 esm 寫法

--- errorHandler.js
+++ errorHandler.js
@@ -1,18 +1,18 @@
-function errorHandler(res) {
+export function errorHandler(res) {
   const headers = {
-    "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, Content-Length, X-Requested-With",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
-    "Content-Type": "application/json",
+    'Access-Control-Allow-Headers':
+      'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json',
   };
   res.writeHead(400, headers);
   res.write(
     JSON.stringify({
-      status: "error",
-      message: "欄位錯誤或無此 id",
+      status: 'error',
+      message: '欄位錯誤或無此 id',
     }),
   );
   res.end();
 }
-module.exports = errorHandler;
+
--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const http = require("http");
-const { v4: uuidv4 } = require("uuid");
-const errorHandler = require("./errorHandler");
+import * as http from 'http';
+import { v4 as uuidv4 } from 'uuid';
+import { errorHandler } from './errorHandler.js';
+
 const todos = [];
+
 const requestListener = function (req, res) {
   const headers = {
-    "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, Content-Length, X-Requested-With",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
-    "Content-Type": "application/json",
+    'Access-Control-Allow-Headers':
+      'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json',
   };
-  let body = "";
-  req.on("data", (chunk) => {
+  let body = '';
+  req.on('data', (chunk) => {
     body += chunk;
   });
 
-  if (req.url === "/todos" && req.method === "GET") {
+  if (req.url === '/todos' && req.method === 'GET') {
     res.writeHead(200, headers);
     res.write(
       JSON.stringify({
-        status: "success",
+        status: 'success',
         data: todos,
       }),
     );
     res.end();
-  } else if (req.url === "/todos" && req.method === "POST") {
-    req.on("end", () => {
+  } else if (req.url === '/todos' && req.method === 'POST') {
+    req.on('end', () => {
       try {
         const { title } = JSON.parse(body);
         if (title !== undefined) {
@@ -33,7 +35,7 @@ const requestListener = function (req, res) {
           res.writeHead(200, headers);
           res.write(
             JSON.stringify({
-              status: "success",
+              status: 'success',
               data: todos,
             }),
           );
@@ -45,25 +47,25 @@ const requestListener = function (req, res) {
         errorHandler(res);
       }
     });
-  } else if (req.url === "/todos" && req.method === "DELETE") {
+  } else if (req.url === '/todos' && req.method === 'DELETE') {
     todos.length = 0;
     res.writeHead(200, headers);
     res.write(
       JSON.stringify({
-        status: "success",
+        status: 'success',
         data: todos,
       }),
     );
     res.end();
-  } else if (req.url.startsWith("/todos/") && req.method === "DELETE") {
-    const todoId = req.url.split("/").pop();
+  } else if (req.url.startsWith('/todos/') && req.method === 'DELETE') {
+    const todoId = req.url.split('/').pop();
     const todoIndex = todos.findIndex((todo) => todo.id === todoId);
     if (todoIndex !== -1) {
       todos.splice(todoIndex, 1);
       res.writeHead(200, headers);
       res.write(
         JSON.stringify({
-          status: "success",
+          status: 'success',
           data: todos,
         }),
       );
@@ -71,18 +73,18 @@ const requestListener = function (req, res) {
     } else {
       errorHandler(res);
     }
-  } else if (req.url.startsWith("/todos/") && req.method === "PATCH") {
-    req.on("end", () => {
+  } else if (req.url.startsWith('/todos/') && req.method === 'PATCH') {
+    req.on('end', () => {
       try {
         const { title } = JSON.parse(body);
-        const todoId = req.url.split("/").pop();
+        const todoId = req.url.split('/').pop();
         const todoIndex = todos.findIndex((todo) => todo.id === todoId);
         if (title !== undefined && todoIndex !== -1) {
           todos[todoIndex].title = title;
           res.writeHead(200, headers);
           res.write(
             JSON.stringify({
-              status: "success",
+              status: 'success',
               data: todos,
             }),
           );
@@ -94,15 +96,15 @@ const requestListener = function (req, res) {
         errorHandler(res);
       }
     });
-  } else if (req.method === "OPTIONS") {
+  } else if (req.method === 'OPTIONS') {
     res.writeHead(200, headers);
     res.end();
   } else {
     res.writeHead(404, headers);
     res.write(
       JSON.stringify({
-        status: "error",
-        message: "無此路由",
+        status: 'error',
+        message: '無此路由',
       }),
     );
     res.end();
@@ -110,6 +112,4 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(process.env.PORT || 3005, () => {
-  console.log("Server is running on http://localhost:3005");
-});
+server.listen(process.env.PORT || 3005);
